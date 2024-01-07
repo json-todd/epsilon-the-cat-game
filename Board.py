1a0bbf817bca16a7cc60ab1b292d7a725015f5a4
json-todd <jsontodd@replit.com>
2024-01-07 18:35:08 +0000

    feat: write template for pieces to be on the Board. Will extend this to Epsion & Food

--- Board.py
+++ Board.py
@@ -6,6 +6,9 @@ class SquareBoard:
         ]
     
     def print(board:list) -> None:
+        """
+        Visualise the board to the terminal
+        """
         for y in board:
             row = '|'  
             for x in y:
@@ -14,10 +17,39 @@ class SquareBoard:
             print(row.rstrip())
 
 
-    def update_obj_pos(self, object, x_obj, y_obj) -> bool:
+class BoardPiece:
+    def __init__(self, name, x_pos: str, y_pos: str, board):
+        """
+        A piece on the board
+        It is placed in the co-ordinate (x, y)
+        where index starts at 0
+
+        param:
+            board: Board, the board the piece is on
+            x_pos: str, the piece's vertical position 
+            y_pos: str, the piece's horizontal position
+        """
+        self.board = board
+        self.name = name
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+
+    def update_pos(self, x_new, y_new) -> bool:
+        """
+        Move the piece on the board
+
+        param:
+            x_new: str, the new vertical position
+            y_new: str, the new horizontal position
+        return:
+            bool, True if the move was successful, False otherwise
+        """
         try:
-            self.board[y_obj][x_obj] = object
-            return 0
-        except Exception as e:
-            print(f'Ur oh. Error occurs {e}')
-            return 1
+            # piece is moved to new location
+            self.board[y_new][x_new] = self.name
+            # and old location is cleared
+            self.board[self.y_pos][self.x_pos] = '  '
+            return True
+        except Exception as error:
+            print(f'Ur oh. Can\'t move the piece.\nThis occurs: {error}')
+            return False
