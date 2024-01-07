b4a5c06fc04b9501404400a817e6b6fa35308362
json-todd <jsontodd@replit.com>
2024-01-07 19:01:51 +0000

    feat: Epsilon constructor, methods to move her around. Also run test on instantiate Epsilon when running the file

--- Epsilon.py
+++ Epsilon.py
@@ -1,27 +1,57 @@
 from random import randint
-import Board
+from Board import SquareBoard, BoardPiece
 
-class Epsilon:
-    def __init__(self, Board):
-        self.x_Eps, self.y_Eps = [randint(0, Board.size - 1) for i in range(2)]
-        self.parent_board = Board
 
-    def update_pos_on_board(self):
-        self.parent_board[self.y_Eps][self.x_Eps] = str(self)
-        
+class Epsilon(BoardPiece):
+
+    def __init__(self, board):
+        """A cute cat. She's the main star of our game
+        Starting the game, her entrance to the board is random
+
+        param:
+            board: Board, the game board Epsilon is placed on
+        """
+        x_Eps, y_Eps = [
+            randint(
+                0, board.size - 1) \
+            for i in range(2)
+        ]
+        super().__init__(str(self), x_Eps, y_Eps, board)
+
     def move_up(self):
-        if self.y_Eps == 0: return
-        
-        self.y_Eps -= 1
+        if self.y_pos == 0: return
+        self.y_pos -= 1
+
     def move_down(self):
         if self.x_Eps == 0: return
         self.x_Eps -= 1
+
     def move_left(self):
-        if self.y_Eps == Board.size - 1: return
-        self.y_Eps += 1
+        if self.y_pos == self.board.size - 1: return
+        self.y_pos += 1
+
     def move_right(self):
-        if self.x_Eps == Board.size - 1: return
-        self.x_Eps += 1    
+        if self.x_Eps == self.board.size - 1: return
+        self.x_Eps += 1
 
     def __str__(self):
         return '🐈'
+
+
+if __name__ == '__main__':
+    board_test = SquareBoard(5)
+
+    for _test in range(10000):
+        try:
+            # print(f'Case #{_test}:')
+            eps_test = Epsilon(board_test)
+    
+            assert all(isinstance(_elem, int) for _elem in eps_test.get_pos())
+            assert all(_elem < board_test.size for _elem in eps_test.get_pos())
+    
+            # print(str(eps_test))
+            # print(eps_test.get_pos())
+            # print()
+        except AssertionError as assert_error:
+            print(f'Failure at #{_test}.\nThis occurs: {assert_error}')
+            print(eps_test.get_pos())
