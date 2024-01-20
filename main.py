802736746ae9c2775c97192c2484706278d5cb8d
json-todd <jsontodd@replit.com>
2024-01-20 11:07:22 +0000

    refactor: get user command with get key so that user wont have to issue Enter

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from random import randint
-import subprocess
+from getkey import getkey, keys
 
 # Board is a square
 # 7x7 board
 BOARD_SIZE = 7
 BOARD = [['  ' for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
-USER_ALLOWED_INPUT = {'w', 'a', 's', 'd'}
+USER_ALLOWED_INPUT = {'w', 'a', 's', 'd', keys.UP, keys.LEFT, keys.DOWN, keys.RIGHT}
 QUIT_GAME_SIGNAL = 'q'
 EXIT_CODE_WIN = 0
 EXIT_CODE_QUIT = 1
@@ -59,31 +59,31 @@ def user_control():
     while user_command != QUIT_GAME_SIGNAL:
         old_x_Eps, old_y_Eps = x_Eps, y_Eps
         print('\nMove Epsilone with [W] [A] [S] [D]:')
-        user_command = input('').lower()
+        user_command = getkey()
 
         if user_command not in USER_ALLOWED_INPUT:
             print("Woops, Epsilon doesn\'t know where to move")
             continue
 
-        if user_command == 'w':
+        if user_command == 'w' or user_command == keys.UP:
             # move Epsilon upward
             if y_Eps == 0:
                 board_print(BOARD)
                 continue
             y_Eps -= 1
-        elif user_command == 'a':
+        elif user_command == 'a' or user_command == keys.LEFT:
             # move Epsilon left-ward
             if x_Eps == 0:
                 board_print(BOARD)
                 continue
             x_Eps -= 1
-        elif user_command == 's':
+        elif user_command == 's' or user_command == keys.DOWN:
             # move Epsilon downward
             if y_Eps == BOARD_SIZE - 1:
                 board_print(BOARD)
                 continue
             y_Eps += 1
-        elif user_command == 'd':
+        elif user_command == 'd' or user_command == keys.RIGHT:
             # move Epsilon right_ward:
             if x_Eps == BOARD_SIZE - 1:
                 board_print(BOARD)
